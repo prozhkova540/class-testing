ade61317a4a2b21860acb87c351ce49514edd36b
polina <prozhkova@uchicago.edu>
2022-03-30 17:39:40 -0500

    Created class example

---
+++ class_1_testing.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+"""Addition (+), subtraction (-), multiplication (*), 
+division (/), modulo (%) and power (**) operators are 
+built into the Python language.
+"""
+print ("Hello World!")
+
+x=10 
+
+print(x*2)
